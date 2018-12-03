c2975a599339cc2b191414f8a622bbea205f4f56
andventuree <awwong92@gmail.com>
2018-12-02 23:48:14 -0500

    11: converted code to make own directive

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,13 +1,17 @@
 "use strict";
 
-angular.module("blogList").controller("BlogListController", function($scope) {
-  console.log("hello");
-  $scope.title = "this is andrews angular app";
-  $scope.clicks = 0;
-  $scope.someClickTest = function() {
-    console.log("Clicking the button");
-    $scope.clicks += 1;
-    $scope.title = "Clicked! " + $scope.clicks + " times";
-  };
+angular.module("blogList").component("blogList", {
+  template:
+    "<div class='new-class' ><h1>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
+  //template does not need ng-controller="BlogListController" anymore
+  controller: function($scope) {
+    console.log("hello");
+    $scope.title = "this is andrews angular app";
+    $scope.clicks = 0;
+    $scope.someClickTest = function() {
+      console.log("Clicking the button");
+      $scope.clicks += 1;
+      $scope.title = "Clicked! " + $scope.clicks + " times";
+    };
+  }
 });
-//.component('blogList')
