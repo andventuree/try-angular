41e6b5e61340f7b52e595dbfca46ad8e13c3522f
andventuree <awwong92@gmail.com>
2018-12-02 23:42:18 -0500

    11: simple button handler

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,6 +1,13 @@
 "use strict";
 
-angular.module("blogList").controller("BlogListController", function() {
+angular.module("blogList").controller("BlogListController", function($scope) {
   console.log("hello");
+  $scope.title = "this is andrews angular app";
+  $scope.clicks = 0;
+  $scope.someClickTest = function() {
+    console.log("Clicking the button");
+    $scope.clicks += 1;
+    $scope.title = "Clicked! " + $scope.clicks + " times";
+  };
 });
 //.component('blogList')
